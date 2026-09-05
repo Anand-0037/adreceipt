50a27840bf71afe9673faab1e490626b0c8c7c37
Anirudh <anirudhpatwal19@gmail.com>
2026-09-05 21:13:37 +0530

    fix(contracts): load .env and record the Sepolia deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL ?? "";
